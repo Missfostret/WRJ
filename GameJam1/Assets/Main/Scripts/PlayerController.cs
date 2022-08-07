255de89697ed67f173c8ca22201d55985ef16da1
RickardDahlberg93 <84345083+RickardDahlberg93@users.noreply.github.com>
2022-08-07 18:48:22 +0200

    Update PlayerController.cs
    Movement Control Update

--- GameJam1/Assets/Main/Scripts/PlayerController.cs
+++ GameJam1/Assets/Main/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    float moveSpeed = 15f;
+    [SerializeField]
+    GameObject reticle;
+    [SerializeField]
+    Rigidbody2D rb;
+
+    float moveRight;
+    float moveUp;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveRight = Input.GetAxisRaw("Horizontal");
+        moveUp = Input.GetAxisRaw("Vertical");
+        
+    }
+    private void FixedUpdate()
+    {
+        if (moveRight != 0f || moveUp != 0f)
+        {
+            if (moveRight != 0f && moveUp != 0f)
+            {
+                var movement = new Vector2(moveRight, moveUp);
+                rb.AddForce(movement * (moveSpeed / Mathf.Sqrt (2)));
+            }
+            else
+            {
+                var movement = new Vector2(moveRight, moveUp);
+                rb.AddForce(movement * moveSpeed);
+            }
+           
+        }
         
     }
 }
