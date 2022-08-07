cc2a153ed733be6cb0e53217ea0b15973ad69a6a
JonisPonis <jontehojo@live.se>
2022-08-07 20:27:17 +0200

    Update PlayerController
    remove whitespaces and empty lines

--- GameJam1/Assets/Main/Scripts/PlayerController.cs
+++ GameJam1/Assets/Main/Scripts/PlayerController.cs
@@ -14,18 +14,10 @@ public class PlayerController : MonoBehaviour
     float moveRight;
     float moveUp;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         moveRight = Input.GetAxisRaw("Horizontal");
         moveUp = Input.GetAxisRaw("Vertical");
-        
     }
     private void FixedUpdate()
     {
@@ -34,15 +26,13 @@ public class PlayerController : MonoBehaviour
             if (moveRight != 0f && moveUp != 0f)
             {
                 var movement = new Vector2(moveRight, moveUp);
-                rb.AddForce(movement * (moveSpeed / Mathf.Sqrt (2)));
+                rb.AddForce(movement * (moveSpeed / Mathf.Sqrt(2)));
             }
             else
             {
                 var movement = new Vector2(moveRight, moveUp);
                 rb.AddForce(movement * moveSpeed);
             }
-           
         }
-        
     }
 }
